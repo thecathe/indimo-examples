5ddf784c468b533a34b30ee82614c02b0ed8ff01
cathe <pearsandcabbage@gmail.com>
2026-05-07 13:57:56 +0100

    moved from outer repo

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [
+    git
+    erlang
+    rebar3
+    erlang-language-platform
+    erlfmt
+    opam
+    go
+    gotools
+  ];
+}
