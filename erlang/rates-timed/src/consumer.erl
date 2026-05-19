5137e9de51c903d8eb733eebef02b10d2ce26e00
cathe <pearsandcabbage@gmail.com>
2026-05-19 12:29:19 +0100

    comments and simplified tick/loop

--- erlang/rates-timed/src/consumer.erl
+++ erlang/rates-timed/src/consumer.erl
@@ -21,7 +21,6 @@
 -define(NAME, rate_consumer).
 %% ticks between queue-depth reports
 -define(REPORT_INTERVAL, 20).
--define(CONSUME_PERIOD, 1).
 
 -record(data, {
     %% msgs/sec we can consume
@@ -48,17 +47,21 @@ start_link(Rate, Tick) ->
 
 callback_mode() -> handle_event_function.
 
+%% 
 init({Rate, Tick}) ->
-    log(init, started, io_lib:format("  rate=~w msg/s   tick_multi=~w", [Rate, Tick])),
+    log(init, started, io_lib:format("  rate=~w msg/t   tick_multi=~w", [Rate, Tick])),
     Data = new_data(Rate, Tick),
+    % start waiting for first tick
     {ok, waiting, Data, tick(Data)}.
 
-handle_event({timeout, tick}, open, _State = waiting, Data) ->
-    {next_state, consume, Data, {state_timeout, ?CONSUME_PERIOD, closed}};
-handle_event(state_timeout, closed, State = consume, Data = #data{tick_count = T}) ->
-    Data2 = Data#data{tick_count = T + 1},
-    maybe_report(State, Data2),
-    {next_state, waiting, Data2, tick(Data2)};
+%% tick = Tick / Rate
+tick(Data) -> {{timeout, tick}, interval(Data), open}.
+
+%% on tick open -> consume
+handle_event({timeout, tick}, open, _State = waiting, Data = #data{tick_count = T}) ->
+    {next_state, consume, Data#data{tick_count = T + 1}};
+
+%% consume work -> wait for next tick
 handle_event(info, {work, _Id, _Seq}, State = consume, Data = #data{tick_count = T, processed = P}) ->
     log(State, consumed, io_lib:format("work {~w, ~w}", [_Id, _Seq])),
     Data1 = Data#data{processed = P + 1, tick_count = T + 1},
@@ -66,13 +69,23 @@ handle_event(info, {work, _Id, _Seq}, State = consume, Data = #data{tick_count =
     maybe_report(State, Data1#data{backlog = dec_backlog(Data#data.backlog)}),
     Data2 = Data1#data{backlog = 0},
     {next_state, waiting, Data2, tick(Data2)};
+
+%% postpone work outside of consume time
 handle_event(info, {work, _Id, _Seq}, State = waiting, Data = #data{backlog = N}) ->
     log(State, postponed, io_lib:format("work {~w, ~w}", [_Id, _Seq])),
     Data2 = Data#data{backlog = N + 1},
     maybe_report(State, Data2),
     {keep_state, Data2, postpone};
+
+%%--------------------------------------------------------------------
+
+%% report backlog to monitor
 handle_event({call, From}, get_backlog, _State, Data) ->
     {keep_state_and_data, {reply, From, Data#data.backlog}};
+
+%%--------------------------------------------------------------------
+
+%% other events
 handle_event({call, _From}, _EventContent, _State, _Data) ->
     keep_state_and_data;
 handle_event(cast, _EventContent, _State, _Data) ->
@@ -97,8 +110,6 @@ log(_State, _Kind = postponed, _Msg) -> ok;
 log(_State, _Kind = consumed, _Msg) -> ok;
 log(State, Kind = report, Msg) -> io:format("[consumer] ~w  (~w)  ~s~n", [Kind, State, Msg]);
 log(_State, Kind, Msg) -> io:format("[consumer] ~w ~s~n", [Kind, Msg]).
-
-tick(Data) -> {{timeout, tick}, interval(Data), open}.
 
 %% Convert a rate (msg/s) to a millisecond interval.
 interval(_Data = #data{rate = Rate, tick_mult = Mult}) ->
