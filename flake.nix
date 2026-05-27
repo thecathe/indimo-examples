c84cd3388f1cd3a56c067b6095a06fc2d8384cae
thecathe <pearsandcabbage@gmail.com>
2026-05-27 08:40:19 +0100

    switch to nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Nix flake for development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          git
+          erlang
+          rebar3
+          erlang-language-platform
+          erlfmt
+          # opam
+          go
+          gotools
+        ];
+      };
+
+    };
+}
